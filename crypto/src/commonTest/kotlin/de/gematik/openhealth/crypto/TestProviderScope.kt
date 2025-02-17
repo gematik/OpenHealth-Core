b5f759b598672ec249b5c8ef113379e326cc3260
Tobias Schwerdtfeger <tobias.schwerdtfeger@gematik.de>
2025-02-17 22:24:37 +0100

    detekt + ktlint

--- crypto/src/commonTest/kotlin/de/gematik/openhealth/crypto/TestProviderScope.kt
+++ crypto/src/commonTest/kotlin/de/gematik/openhealth/crypto/TestProviderScope.kt
@@ -46,6 +46,6 @@ fun runTestWithProvider(
     ) {
         useCryptoAsync {
             initializeNativeCryptoProvider()
-            testBody(TestProviderScope(this@runTest, this@useCryptoAsync)) // sdgsdfg
+            testBody(TestProviderScope(this@runTest, this@useCryptoAsync))
         }
     }
