2657bd0b71c60cf1804f129b4612946df79b35e6
Tobias Schwerdtfeger <this@tobias-schwerdtfeger.dev>
2025-11-27 15:23:55 +0100

    OPEN-86: Add SPDX license headers across all files and include CC0-1.0 license for Gradle wrapper

--- core-modules-kotlin/gradlew.bat
+++ core-modules-kotlin/gradlew.bat
@@ -1,3 +1,24 @@
+REM SPDX-FileCopyrightText: Copyright 2025 gematik GmbH
+REM
+REM SPDX-License-Identifier: Apache-2.0
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+REM
+REM *******
+REM
+REM For additional notes and disclaimer from gematik and in case of changes by gematik,
+REM find details in the "Readme" file.
+
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
